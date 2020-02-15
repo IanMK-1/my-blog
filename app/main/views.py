6b33b9a941359c1ab69cdd6214622f9a1917fb1f
ian mwangi <iankimotho233@gmail.com>
2020-02-15 12:42:11 +0300

    Create a route that returns a quote instance to index.html

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from ..request import obtain_quote
+from . import main
+
+
+@main.route('/')
+def index():
+    quote = obtain_quote()
+
+    return render_template('index.html', quote=quote)
