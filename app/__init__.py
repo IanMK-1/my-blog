ec51a6ad6fe0257db0a965c0043c234b6d89810d
ian mwangi <iankimotho233@gmail.com>
2020-02-15 12:37:03 +0300

    Create a main blueprint and register it

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,10 @@ def create_app(config_name):
     """Initializing the extensions"""
     bootstrap.init_app(app)
 
+    """Register the blueprints"""
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     from .request import configure_request
     configure_request(app)
 
