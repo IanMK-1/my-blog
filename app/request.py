4992039bf4837129bcbd19095507df5fdb974110
ian mwangi <iankimotho233@gmail.com>
2020-02-15 12:27:31 +0300

    Create a function that obtains quotes

--- app/request.py
+++ app/request.py
@@ -7,3 +7,19 @@ quotes_url = None
 def configure_request(app):
     global quotes_url
     quotes_url = app.config['QUOTES_URL']
+
+def obtain_quote():
+    """Obtains the quotes from the url"""
+
+    with urllib.request.urlopen(quotes_url) as source_url:
+        get_quote = source_url.read()
+        get_response = json.loads(get_quote)
+
+        results = None
+
+        if get_response:
+            response = get_response
+            results = process_results(response)
+
+        return results
+
