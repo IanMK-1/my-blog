b11217c73a5a190bf902a73f13feecedb08836dc
ian mwangi <iankimotho233@gmail.com>
2020-02-15 19:57:38 +0300

    Create a table for users

--- app/models.py
+++ app/models.py
@@ -26,3 +26,27 @@ class Writer(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.writer_password, password)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    user_password = db.Column(db.String(255))
+
+    # return a printable representation of the object
+    def __repr__(self):
+        return f'User{self.username}'
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.user_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.user_password, password)
