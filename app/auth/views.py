ea67acaba76a30843e3d0848bd224b42cb91c7d5
ian mwangi <iankimotho233@gmail.com>
2020-02-16 12:20:58 +0300

    Add route for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,10 +10,25 @@ from flask_login import login_user, login_required, logout_user
 def signup():
     form = WriterRegistration()
     if form.validate_on_submit():
-        writer = Writer(full_names=form.full_names.data, email=form.email.data,writer_password=form.writer_password.data)
+        writer = Writer(full_names=form.full_names.data, email=form.email.data,
+                        password=form.writer_password.data)
         db.session.add(writer)
         db.session.commit()
 
         return redirect(url_for('auth.writer_login'))
 
-    return render_template('user_auth/signup.html', signup_form=form)
+    return render_template('writer_auth/signup.html', signup_form=form)
+
+
+@auth.route('/login', methods=["GET", "POST"])
+def writer_login():
+    login_form = WriterLogin()
+    if login_form.validate_on_submit():
+        writer = Writer.query.filter_by(email=login_form.email.data).first()
+        if writer is not None and writer.verify_password(login_form.writer_password.data):
+            login_user(writer, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or password')
+
+    return render_template('writer_auth/login.html', writerlogin_form=login_form)
